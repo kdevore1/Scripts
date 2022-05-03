192d76cba4baf11baafd37f0de685099eb16e8ec
Chiu Laboratory <plchiu@asu.edu>
2022-05-03 09:37:49 -0700

    Update install_packages_workstation.sh

--- install_packages_workstation.sh
+++ install_packages_workstation.sh
@@ -27,6 +27,9 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 cd ${current_dir}
 
+
+
+
 # 6. Common packages that are used for structural biology
 #   CCP4, Phenix, UCSF Chimera, UCSF ChimeraX, 
 #
