4a265085e903f02e9398f281f20b41ceba89a3da
Chiu Laboratory <plchiu@asu.edu>
2022-05-03 09:33:16 -0700

    Update install_packages_workstation.sh

--- install_packages_workstation.sh
+++ install_packages_workstation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+current_dir=`pwd`
+
 # 1. Install the Nvidia graphic card driver
 # sudo ubuntu-drivers devices
 # sudo ubuntu-drivers autoinstall
@@ -7,8 +9,14 @@
 # 2. Download CUDA and install the run file
 
 # 3. Use apt to install required packages
-sudo apt install build-essential cmake git libtiff-dev libtiff5-dev mpich
+sudo apt install build-essential cmake git libtiff-dev libtiff5-dev mpich curl
 
-# 4. Common packages that are used for structural biology
+# 4. Compile FFTW library
+cd ~/Downloads
+wget https://www.fftw.org/fftw-3.3.10.tar.gz
+tar zxvf fftw-3.3.10.tar.gz
+
+
+# 5. Common packages that are used for structural biology
 #   CCP4, Phenix, UCSF Chimera, UCSF ChimeraX, 
 #
