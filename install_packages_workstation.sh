7af7c737ea0b25522b6be63d98fd70221b499159
Chiu Laboratory <plchiu@asu.edu>
2022-05-03 09:27:51 -0700

    Create install_packages_workstation.sh

---
+++ install_packages_workstation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 1. Install the Nvidia graphic card driver
+# sudo ubuntu-drivers devices
+# sudo ubuntu-drivers autoinstall
+
+# 2. Use apt to install required packages
+sudo apt install build-essential cmake git libtiff-dev libtiff5-dev 
