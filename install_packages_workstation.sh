69eaaca3684ebe40b83abf8c6b697ca8359a77f3
Chiu Laboratory <plchiu@asu.edu>
2022-05-03 09:28:56 -0700

    Update install_packages_workstation.sh

--- install_packages_workstation.sh
+++ install_packages_workstation.sh
@@ -4,5 +4,9 @@
 # sudo ubuntu-drivers devices
 # sudo ubuntu-drivers autoinstall
 
-# 2. Use apt to install required packages
+# 2. Download CUDA and install the run file
+
+# 3. Use apt to install required packages
 sudo apt install build-essential cmake git libtiff-dev libtiff5-dev 
+
+
