293d88e03f6094e57f37650e635b8921940c90e1
Kira <80859023+kdevore1@users.noreply.github.com>
2022-11-22 10:40:13 -0700

    Update generate_2D_shift_histograms.py

--- box_size_metadata/calculate_2D_shift/generate_2D_shift_histograms.py
+++ box_size_metadata/calculate_2D_shift/generate_2D_shift_histograms.py
@@ -2,7 +2,7 @@
 # The purpose of this script is to create histograms of 2D shifts from cryoSPARC metadata (csv files)
 
 # imports
-import numpy as np
+import numpy as n
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,6 +19,10 @@ image_list = ['P49_box_1.0_2D_shift.png', 'P49_box_1.5_2D_shift.png', 'P49_box_2
 
 
 calculated_shift_list = []
+
+mean_list = []
+std_list = []
+var_list = []
 
 # loops through each csv file in the list, calculates the sqrt(x^2+y^2) for each row of df and appends to a list, 
 for file, image in zip(file_list, image_list):
@@ -37,3 +41,22 @@ for file, image in zip(file_list, image_list):
     plt.title('Shifts')
     plt.savefig(image, dpi = 1200)
     plt.clf()
+    
+    # calculate summary statistics (mean, sigma, and variance) and write to separate lists
+    mean = n.mean(calculated_shift_list)
+    mean_list.append(mean)
+    std = n.std(calculated_shift_list)
+    std_list.append(std)
+    var = n.var(calculated_shift_list)
+    var_list.append(var)
+    
+    
+# zip summary statistics lists
+combined_list = zip(mean_list, std_list, var_list)
+
+# header for csv file with summary statistics
+sum_stat_header = ['Mean', 'Standard Deviation', 'Variance']
+
+# convert to data frame and save as csv
+df2 = pd.DataFrame(combined_list, columns = ['mean_list', 'std_list', 'var_list'])
+df2.to_csv('sum_stat.csv', index = False, header = sum_stat_header)
